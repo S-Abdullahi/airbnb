fc564dd09a242d6801f12c43f7187bad682de406
Abdullahi Salawu <salabdullahi33@gmail.com>
2023-01-14 23:40:25 +0100

    airbnb clone first commit

---
+++ src/component/Nav.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Nav(){
+    return (
+        <div className="image-con">
+        
+        <img src={require("../images/airbnb.png")} alt="airbnb" className="nav"/>
+    </div>
+    )
+}
