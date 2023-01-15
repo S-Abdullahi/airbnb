2df75b39429c6f08f2c286444e3ccb4d9fa3bbd4
Abdullahi Salawu <salabdullahi33@gmail.com>
2023-01-15 14:11:20 +0100

    adding data

--- src/component/Card.js
+++ src/component/Card.js
@@ -1,19 +1,22 @@
 import React from 'react'
 
-export default function (){
+export default function Card({img,rating,reviewCount, country, title, price, openSpot}){
+
+    
+
     return (
         <div  className='product'>
-            <img src={require("../images/image12.png")} className='product__image'/>
+            <img src={require(`../images/${img}`)} className='product__image'/>
             <div className='product__detail'>
                 <img src={require("../images/star1.png")} className="product__star"/>
-                <span className='product__rating'>5.0</span>
-                <span className='product__quantity'>(6)</span>
+                <span className='product__rating'>{rating}</span>
+                <span className='product__quantity'>({reviewCount})</span>
                 <div className='dot'></div>
-                <span className='product__country'>USA</span>
+                <span className='product__country'>{country}</span>
             </div>
-            <p className='product__description'>Life lessons with Katie Zaferes</p>
-            <span className='product__price'>From $136 / <span className='person'>person</span></span>
-            <span className='label'>sold out</span>
+            <p className='product__description'>{title}</p>
+            <span className='product__price'>From ${price} / <span className='person'>person</span></span>
+            <span className='label' style={{display: openSpot == 0 ? 'block' : 'none'}}>sold out</span>
         </div>
     )
 }
--- src/index.js
+++ src/index.js
@@ -1,22 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {createRoot} from 'react-dom/client'
 import Nav from './component/Nav'
 import Hero from './component/Hero';
 import Card from './component/Card';
+import Datas from './Data'
 
 //import css
 import './style.css'
 
 function App(){
+  
   return (
     <>
       <Nav/>
       <Hero/>
       <section className='products-con'>
-          <Card/>
+          {Datas.map((data)=>{
+            return <Card 
+                  key = {data.id}
+                  img={data.coverImg}
+                  rating={data.stats.rating.toFixed(1)} reviewCount={data.stats.reviewCount} 
+                  country={data.location} 
+                  title={data.title} 
+                  price={data.price}
+                  openSpot={data.openSpots}/>
+          })}
       </section>
     </>
   )
 }
 
-ReactDOM.render(<App />, document.getElementById('root'))
+const container = document.getElementById('root')
+const root = createRoot(container)
+root.render(<App />)
